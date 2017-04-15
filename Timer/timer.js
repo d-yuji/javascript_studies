965f88840fcb81a84c507b2a4e6fea6bfbedbfeb
d.yuji <bronzsord@gmail.com>
2017-04-15 17:56:25 +0900

    start,stopの実装

--- Timer/timer.js
+++ Timer/timer.js
@@ -2,19 +2,23 @@
 	'use strict';
 	var startTime;
 	var timerId;
+	var elapsedTime = 0;
 	var startButton = document.getElementById("start");
 	var stopButton = document.getElementById("stop");
 	var resetButton = document.getElementById("reset");
 	var timerText = document.getElementById("timerText");
 
-	startButton.addEventListener('click',function(){
+	startButton.addEventListener('click', function(){
 		startTime = Date.now();
 		updateTimerText();
 	});
-
+	stopButton.addEventListener('click', function(){
+		elapsedTime += Date.now() - startTime;
+		clearTimeout(timerId);
+	});
 	function updateTimerText(){
 		timerId = setTimeout(function(){
-			var t = Date.now() - startTime;
+			var t = Date.now() - startTime + elapsedTime;
 			timerText.innerHTML = ( t / 1000).toFixed(2);
 			updateTimerText();
 		}, 10);
