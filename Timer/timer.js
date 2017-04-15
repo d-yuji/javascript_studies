cb3da3cb9977b2c547d449602de0a4ad3e69593d
d.yuji <bronzsord@gmail.com>
2017-04-15 15:06:51 +0900

    スタートボタンのスクリプト

--- Timer/timer.js
+++ Timer/timer.js
@@ -1,7 +1,22 @@
 (function(){
 	'use strict';
+	var startTime;
+	var timerId;
 	var startButton = document.getElementById("start");
 	var stopButton = document.getElementById("stop");
 	var resetButton = document.getElementById("reset");
 	var timerText = document.getElementById("timerText");
+
+	startButton.addEventListener('click',function(){
+		startTime = Date.now();
+		updateTimerText();
+	});
+
+	function updateTimerText(){
+		timerId = setTimeout(function(){
+			var t = Date.now() - startTime;
+			timerText.innerHTML = ( t / 1000).toFixed(2);
+			updateTimerText();
+		}, 10);
+	}
 })();
