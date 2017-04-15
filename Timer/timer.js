e25aade1222b0b83ba0a19e0f17446ee44063e38
d.yuji <bronzsord@gmail.com>
2017-04-15 15:01:15 +0900

    jsの作成、idの要素取得

---
+++ Timer/timer.js
@@ -0,0 +1,7 @@
+(function(){
+	'use strict';
+	var startButton = document.getElementById("start");
+	var stopButton = document.getElementById("stop");
+	var resetButton = document.getElementById("reset");
+	var timerText = document.getElementById("timerText");
+})();
