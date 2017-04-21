6a2819020963c3ffda9224b7bb4da6cefa7e8963
d.yuji <bronzsord@gmail.com>
2017-04-21 13:13:27 +0900

    リファクタリング

--- Omikuzi/omikuzi.js
+++ Omikuzi/omikuzi.js
@@ -1,17 +1,20 @@
 (function() {
 	"use strict";
-	document.getElementById('button').addEventListener('click',function () {
+	var Button = document.getElementById('button');
+	var Result = document.getElementById('results');
+	Button.addEventListener('click',function () {
 		var omikuziResults = ['大吉','中吉','末吉','小吉','凶','大凶'];
 		var Colors = ['red','orange','lime','green','blue','purple'];
 		var result = Math.floor(Math.random()*omikuziResults.length);
-		// document.getElementById('results').innerHTML = 'hit';
-		document.getElementById('results').innerHTML = omikuziResults[result];
-		document.getElementById("results").style.backgroundColor = Colors[result];
+		Result.innerHTML = omikuziResults[result];
+		Result.style.backgroundColor = Colors[result];
 	});
-	document.getElementById('button').addEventListener('mousedown',function () {
+
+	Button.addEventListener('mousedown',function () {
 		this.className = 'pushed';
 	});
-	document.getElementById('button').addEventListener('mouseup',function () {
+
+	Button.addEventListener('mouseup',function () {
 		this.className = '';
 	});
 })();
